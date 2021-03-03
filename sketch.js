dff6bf7fccda08cd124400bc5cb8e006ce758f61
CHILLBRLMO <75742564+CHILLBRLMO@users.noreply.github.com>
2021-03-03 13:39:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var car1;
+var car2;
+var car3;
+var car4;
+var cars;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-50,displayHeight-50);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+  
+
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
